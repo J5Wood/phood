3b3afe949d3a687032f9a8c3c5b361cc955bfc5d
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-11-22 10:41:39 -0800

    Added babel file

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -38,7 +38,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public", "index.html"),
-      //   favicon: "./public/favicon.ico",
+      favicon: "./public/favicon.ico",
       filename: "index.html",
       manifest: "./public/manifest.json",
     }),
