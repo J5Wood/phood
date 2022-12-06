ad6dacd6d56f4c6e788642af0bbb538b2430a3c1
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-12-06 01:10:35 -0800

    Set up CORS for vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import "./App.css";
 
 function App() {
+  async function fetchData() {
+    const resp = await fetch("http://localhost:3001/restaurants");
+    const data = await resp.json();
+
+    console.log(data.data);
+  }
+
+  fetchData();
+
   return <div>Hello World</div>;
 }
 
