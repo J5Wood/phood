dfbf476a709c451ab3a0032bd0a89283c7e037a8
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-12-08 00:01:22 -0800

    Added header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Header } from "./Header";
 
 function App() {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <div className="header">
+      <h1>PHOOD</h1>
+    </div>
+  );
+}
