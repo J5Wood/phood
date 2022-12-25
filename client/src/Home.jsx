014becb8c6b731cda1c77984487245f26b1ab19a
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2022-12-24 21:48:15 -0800

    Made images for image addition navigation selection

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -7,6 +7,27 @@ export function Home() {
 
   return (
     <div className="home-page">
+      <div className="photo-method-selection">
+        <span>
+          <h4>Take Photo</h4>
+          <div className="icon">
+            <div className="camera-outer-ring">
+              <div className="camera-inner-ring">
+                <div className="camera-lens-flare"></div>
+              </div>
+            </div>
+          </div>
+        </span>
+        <span>
+          <h4>Add From Files</h4>
+          <div className="icon">
+            <div className="file-upper-tab-container">
+              <div className="file-upper-tab"></div>
+            </div>
+            <div className="file-main-body"></div>
+          </div>
+        </span>
+      </div>
       <button className="home-page-button" onClick={(e) => addPhoto(e)}>
         ADD A PHOTO
       </button>
