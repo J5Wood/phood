9eeb57e9adb40f5bc6ddb619a46ec6758267a987
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2023-01-10 20:44:47 -0800

    Added location marker component

---
+++ client/src/LocationMarker.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Marker, Popup, useMapEvents } from "react-leaflet";
+
+export function LocationMarker() {
+  const [position, setPosition] = useState(null);
+  const map = useMapEvents({
+    click() {
+      map.locate();
+    },
+    locationfound(e) {
+      debugger;
+      setPosition(e.latlng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  return position === null ? null : (
+    <Marker position={position}>
+      <Popup>You are here</Popup>
+    </Marker>
+  );
+}
--- client/src/MapWrapper.jsx
+++ client/src/MapWrapper.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { MapContainer, TileLayer, useMap } from "react-leaflet";
-import { Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { LocationMarker } from "./LocationMarker";
 // get image, dish name, amd location info. Send to backend with user token.
 
 export function MapWrapper() {
@@ -10,17 +10,13 @@ export function MapWrapper() {
       className="map"
       center={[51.505, -0.09]}
       zoom={13}
-      scrollWheelZoom={false}
+      scrollWheelZoom={true}
     >
-      <TileLayer
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      /> */}
+      <LocationMarker />
     </MapContainer>
   );
 }
