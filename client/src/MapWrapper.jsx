c74df85aee98c67b0b287b3b4561d7927a1e2058
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2023-01-13 22:10:49 -0800

    Added search input to map page

--- client/src/MapWrapper.jsx
+++ client/src/MapWrapper.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { LocationMarker } from "./LocationMarker";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
+
 // get image, dish name, amd location info. Send to backend with user token.
 
 export function MapWrapper() {
+  const provider = new OpenStreetMapProvider();
+  // const results = await provider.search({ query: input.value });
+
+  const [searchInput, setSearchInput] = useState("");
+
+  function handleInputChange(e) {
+    setSearchInput(e.target.value);
+    mapSearch(e.target.value);
+  }
+
+  async function mapSearch(location) {
+    const results = await provider.search({ query: location });
+    console.log(results);
+  }
+
   return (
-    <MapContainer
-      className="map"
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={true}
-    >
-      {/* <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      /> */}
-      <LocationMarker />
-    </MapContainer>
+    <>
+      <MapContainer
+        className="map"
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LocationMarker />
+      </MapContainer>
+      <input
+        type="text"
+        name="searchInput"
+        id="searchInput"
+        value={searchInput}
+        onChange={(e) => handleInputChange(e)}
+      />
+    </>
   );
 }
