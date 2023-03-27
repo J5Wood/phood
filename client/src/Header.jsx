043f05be8ce1f24abc1e8ea3e9568bfa70f9aa4c
Jeremy Wood <68167282+J5Wood@users.noreply.github.com>
2023-03-26 21:05:42 -0700

    Added header link to home. Changed styling

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export function Header() {
   return (
     <div className="header">
-      <h1>PHOOD</h1>
+      <button id="main-logo" onClick={() => (window.location.href = "/home")}>
+        <h1>PHOOD</h1>
+      </button>
     </div>
   );
 }
